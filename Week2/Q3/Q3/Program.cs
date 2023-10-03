d0e0f500f877d4d3042604d033f7ac472e52640e
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-10-03 20:02:54 +0100

    Q3 Comments
    Added the comments for the q3 of week 2.

--- Week2/Q3/Q3/Program.cs
+++ Week2/Q3/Q3/Program.cs
@@ -1,17 +1,22 @@
-﻿namespace Q3
+﻿using System.Runtime.CompilerServices;
+
+namespace Q3
 {
     internal class Program
     {
+        //This method calculates the area of a circle with a input arg of radius and returns the area in a double type.
         static double CircleArea(double radius)
         {
             double area = Math.Pow(radius, radius) * Math.PI;
             return area;
         }
+        //This method calculates the volume of a cylinder with two input args of radius and height and returns the volume in a double type. .
         static double CylinderVolume(double radius, double height)
         {
             double vol = height * Math.Pow(radius, radius) * Math.PI;
             return Math.Round(vol);
         }
+        //This method prints the menu and gives the options to the user on the console and is a void type.
         static void PrintMenu()
         {
             Console.WriteLine("Select one calculation:" +
@@ -19,12 +24,14 @@
                 "\n2.Volume of Cylinder" +
                 "\n0.Exit application");
         }
+        //This method gets a user input of the option selected and returns the input.
         static int GetOption()
         {
             Console.WriteLine("Input option:");
             int option = Convert.ToInt32(Console.ReadLine());
             return option;
         }
+        //This method uses the option's number value as the input and outputs the values according to the option selected by the user.
         static void GetValue(int n)
         {
             switch (n)
@@ -54,12 +61,12 @@
         }
         static void Main(string[] args)
         {
-            int option = 0;
+            int option = 0;//initializing the option variable
             do
             {
-                PrintMenu();
-                option = GetOption();
-                GetValue(option);
+                PrintMenu();//Calling the print menu method
+                option = GetOption();//Calling the get option method and storing it into a variable
+                GetValue(option);//Calling the get value method and using the variable.
             } while (option != 0);
         }
     }
