52289a75fe6b9a47f71f5f8425e6ff8a9e69e60d
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-10-03 14:50:41 +0100

    Adding Comments
    Adding comments to my Q1 code.

--- Week2/Q1/Q1/Program.cs
+++ Week2/Q1/Q1/Program.cs
@@ -2,6 +2,7 @@
 {
     internal class Program
     {
+        //Creating a Print Menu method for the menu to be printed for the user. This method is void and prints out the menu as it is.
         static void PrintMenu()
         {
             Console.WriteLine("Please enter an option from below:" +
@@ -11,12 +12,14 @@
                     "\n4.Hello in Irish?" +
                     "\n0.Exit the application");
         }
+        //Creating a Get Option method for reading and storing the option selected by the user after asking them to enter the option into the console.
         static int GetOption() 
         {
             Console.WriteLine("Input option:");
             int option = Convert.ToInt32(Console.ReadLine());
             return option;
         }
+        //Creating a Get Message method which has a number arguement i.e. option from GetOption and uses that input in a switch code to return the required greeting to the console.
         static string GetMessage(int n)
         {
             switch (n)
@@ -44,12 +47,13 @@
         static void Main(string[] args)
         {
             int option = 0;
+            //Creating and using a do while loop
             do
             {
-                PrintMenu();
-                option = GetOption();
-                string greeting = GetMessage(option);
-                Console.WriteLine(greeting);
+                PrintMenu(); //Calling the print menu method
+                option = GetOption(); //Calling the get option method and storing its value in a variable
+                string greeting = GetMessage(option); //Calling the get message method and storing it in a variable
+                Console.WriteLine(greeting); //Printing the greeting to the console.
             } while (option != 0);
            
         }
