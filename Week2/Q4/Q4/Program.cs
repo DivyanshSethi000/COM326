2c60afdf3ac66ebc9afd388b1fc1dd2c116b2fe5
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-10-03 23:14:57 +0100

    Q4 of Week 2
    Solution Code of q4 for Week 2

--- Week2/Q4/Q4/Program.cs
+++ Week2/Q4/Q4/Program.cs
@@ -2,9 +2,29 @@
 {
     internal class Program
     {
+        static double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                Console.WriteLine("Input the numerator: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Input the denominator: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                double result = Division(a, b);
+                Console.WriteLine($"Result: {result}");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Exception Caught: " + ex.Message);
+            }
         }
     }
 }
