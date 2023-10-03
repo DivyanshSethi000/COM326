4d78ec788fef824f6848959a5455323ea1e97b81
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-10-03 14:23:51 +0100

    Week 2 Challenge 1
    Committing Q1 of Week2 coding challenges.

---
+++ Week2/Q1/Q1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Q1
+{
+    internal class Program
+    {
+        static void PrintMenu()
+        {
+            Console.WriteLine("Please enter an option from below:" +
+                    "\n1.Hello in French?" +
+                    "\n2.Hello in Spanish?" +
+                    "\n3.Hello in German?" +
+                    "\n4.Hello in Irish?" +
+                    "\n0.Exit the application");
+        }
+        static int GetOption() 
+        {
+            Console.WriteLine("Input option:");
+            int option = Convert.ToInt32(Console.ReadLine());
+            return option;
+        }
+        static string GetMessage(int n)
+        {
+            switch (n)
+            {
+                case 0:
+                    return "Goodbye!";
+                    break;
+                case 1:
+                    return "Bonjour!";
+                    break;
+                case 2:
+                    return "Hola!";
+                    break;
+                case 3:
+                    return "Hallo!";
+                    break;
+                case 4:
+                    return "Dia Dhuit!";
+                    break;
+                default:
+                    return "Please enter a valid option";
+                    break;
+            }
+        }
+        static void Main(string[] args)
+        {
+            PrintMenu();
+            string greeting = GetMessage(GetOption());
+            Console.WriteLine(greeting);
+        }
+    }
+}
