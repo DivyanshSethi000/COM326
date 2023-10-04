d4fad4d1c89f880c23c6302f89f86afc94ebb06f
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-10-04 09:40:54 +0100

    Comment for Q4 for Week2
    Adding the comments of Q4(Challenge 4) for week2.

--- Week2/Q4/Q4/Program.cs
+++ Week2/Q4/Q4/Program.cs
@@ -2,7 +2,19 @@
 {
     internal class Program
     {
+        /*This method is a return double type division accepting 2 input arguments
+        including an exception throw which stops the program from crashing.*/
         static double Division(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            return a / b;
+        }
+        /*This method is a return int type division accepting 2 input arguments
+        including an exception throw which stops the program from crashing.*/
+        static int Division(int a, int b)
         {
             if (b == 0)
             {
@@ -12,18 +24,18 @@
         }
         static void Main(string[] args)
         {
-            try
+            try //starting the try block
             {
-                Console.WriteLine("Input the numerator: ");
-                double a = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Input the denominator: ");
-                double b = Convert.ToDouble(Console.ReadLine());
-                double result = Division(a, b);
-                Console.WriteLine($"Result: {result}");
+                Console.WriteLine("Input the numerator: ");//Input for numerator
+                var a = Convert.ToDouble(Console.ReadLine());//Storing numerator in a variable using var type
+                Console.WriteLine("Input the denominator: ");//Input for denominator
+                var b = Convert.ToDouble(Console.ReadLine());//Storing denominator in a variable using var type
+                var result = Division(a, b);//Calling the method and using var type so that it automatically uses the required method.
+                Console.WriteLine($"Result: {result}");//Outputting the result
             }
             catch (Exception ex) 
             {
-                Console.WriteLine("Exception Caught: " + ex.Message);
+                Console.WriteLine("Exception Caught: " + ex.Message);//Catching any exception and outputting a required message
             }
         }
     }
