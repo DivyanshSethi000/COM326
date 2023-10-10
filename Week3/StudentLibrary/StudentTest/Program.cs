c88d9ad846a1bf1c416b1229ede472f880a6f565
DivyanshSethi000 <122283007+DivyanshSethi000@users.noreply.github.com>
2023-10-10 15:12:06 +0100

    Week 3 Task 1

---
+++ Week3/StudentLibrary/StudentTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using StudentLibrary;
+
+namespace StudentTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
