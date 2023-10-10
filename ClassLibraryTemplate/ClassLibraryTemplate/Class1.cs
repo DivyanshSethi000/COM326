6cbbde2b244e3abc56d5ddac1eeabacd4c1c5b55
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-10-10 14:13:53 +0100

    Adding Class Template

---
+++ ClassLibraryTemplate/ClassLibraryTemplate/Class1.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Classname or filename)
+ *  
+ * Version information
+ * Author:
+ * Date:
+ * 
+ * Copyright notice
+ */
+namespace ClassLibraryTemplate
+{
+    public class Class1
+    {
+        //Add Fields
+
+        //Add Properties
+
+        //Add Constructors
+
+        //Add Methods
+    }
+}
