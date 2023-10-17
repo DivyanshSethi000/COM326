0eb97052c5ec76acbad5884dde04cba6997317f0
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-10-17 14:28:29 +0100

    Challenge 1 Week 4
    Completed the challenge 1 with all tasks and comments

--- Week4/MyArrayProject/MyArrayProject/Program.cs
+++ Week4/MyArrayProject/MyArrayProject/Program.cs
@@ -23,13 +23,52 @@
             {
                 if (alphabet[i] != 'a') //checking if the alphabet is not equal to 'a'
                 {
-                    Console.Write(alphabet[i] + ", "); //adding a coma in between the alphabets in the array
+                    Console.Write(alphabet[i] + ", "); //adding a comma in between the alphabets in the array
                 }
                 else
                 {
                     Console.WriteLine(alphabet[i]);//finishing the array with the alphabet without any comma
                 }
             }
+            for(int i = 0;i < alphabet.Length;i++) //Initializing a third for loop
+            {
+                alphabet[i] = char.ToUpper(alphabet[i]); //converting the alphabets to uppercase
+                if (alphabet[i] != 'Z') //checking if the alphabet is not equal to 'Z'
+                {
+                    Console.Write(alphabet[i] + ", "); //adding a comma in between the alphabets in the array
+                }
+                else
+                {
+                    Console.Write(alphabet[i]); //finishing the array with the alphabet without any comma
+                }
+            }
+
+            int[] fibonacci = { 13, 1, 2, 3, 5, 8, 1, 34, 21, 55 }; //initializing the array with a set of values
+            int max = 0; //initializing a max value to 0
+            foreach (int i in fibonacci) //starting a for each loop
+            {
+                if (i > max) //checking if index is greater than max value
+                {
+                    max = i; //replacing max value with index 
+                }
+            }
+            Console.WriteLine($"Max value is: {max}"); //printing out the max value
+            Array.Sort(fibonacci); //sorting the array in ascending order
+            foreach (int i in fibonacci) //starting a for each loop
+            {
+                Console.Write(i + " "); //printing the values with space in between
+            }
+            int numToFind = 34; //number to find in the array
+            int index = -1; //setting the index to -1 if the number is not found
+            foreach (int i in fibonacci) //starting a for each loop
+            {
+                if (fibonacci[i] == numToFind) //checking if the values in the array match the number to find
+                {
+                    index = i; //setting the index to the index value of the number
+                    break;
+                }
+            }
+            Console.WriteLine($"\nIndex of number: {index}"); //printing out the index of the number in the array
         }
     }
 }
