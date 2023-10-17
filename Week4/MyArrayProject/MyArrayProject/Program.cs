df72ad4e597d118367221b7187390c641cb800cf
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-10-17 13:11:04 +0100

    Week 4 Task 1 - 4
    Finishing the first 4 tasks in the 1st challenge.

--- Week4/MyArrayProject/MyArrayProject/Program.cs
+++ Week4/MyArrayProject/MyArrayProject/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Initializing the char array to store the alphabets
+            char[] alphabet = new char[26];
+            for(int i = 0; i < alphabet.Length; i++) // initializing the for loop 
+            {
+                alphabet[i] = (char)('a' + i); //storing the alphabets in the array with each loop and then converting the numeric value back to character
+                if (alphabet[i] != 'z') //checking if the alphabet is not equal to 'z'
+                {
+                    Console.Write(alphabet[i] + ", "); //adding a coma in between the alphabets in the array
+                }
+                else
+                {
+                    Console.WriteLine(alphabet[i]);//finishing the array with the last alphabet without any comma
+                }
+            }
+
+            for (int i = (alphabet.Length - 1);i >= 0; i--) //Initializing a second for loop
+            {
+                if (alphabet[i] != 'a') //checking if the alphabet is not equal to 'a'
+                {
+                    Console.Write(alphabet[i] + ", "); //adding a coma in between the alphabets in the array
+                }
+                else
+                {
+                    Console.WriteLine(alphabet[i]);//finishing the array with the alphabet without any comma
+                }
+            }
         }
     }
 }
