2805b18683d35edc4a047e51a11f8d1752a5b736
DivyanshSethi000 <122283007+DivyanshSethi000@users.noreply.github.com>
2023-10-24 11:13:31 +0100

    Update Program.cs
    Updated the program for Week 3 Car Test to add a new constructor created in the CarLibrary.

--- Week3/CarLibrary/CarTest/Program.cs
+++ Week3/CarLibrary/CarTest/Program.cs
@@ -8,8 +8,14 @@ namespace CarTest
         {
             Engine engine = new Engine(4 , 210); //instantiating a new engine object using the class Engine and setting its two parameters
             Car car = new Car("Ford", "Fiesta", 2016, engine); //instantiating a new car object using the class Car and setting its parameters
+
             car.Drive();//calling the drive method in the class Car on the car object
             car.Stop();//calling the stop method in the class Car on the car object
+            
+            Car car2 = new Car("Koenigsegg", "Gemera", 2023, 3, 1400); //instantiating a new car object using the class Car and setting its parameters
+
+            car2.Drive();//calling the drive method in the class Car on the car object
+            car2.Stop();//calling the stop method in the class Car on the car object
         }
     }
 }
