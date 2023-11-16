10ca7c139cd559e6a3812735b62cd2e749694543
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-11-16 00:41:49 +0000

    Challenge 2 Week8
    Created a library for the 2nd challenge in week8

---
+++ Week8/MyInheritanceLibrary/ShapeLibrary/Shape.cs
@@ -0,0 +1,14 @@
+﻿namespace ShapeLibrary
+{
+    public abstract class Shape
+    {
+        public abstract string Name { get; set; }
+        public abstract string Color { get; set; }
+
+        public Shape() { }
+
+        public Shape(string name, string color)
+        {
+        }
+    }
+}
