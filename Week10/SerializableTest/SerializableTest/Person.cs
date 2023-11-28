5c3d74faeaf93ced29cc2c5f2f46ed6d3e204d89
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-11-28 17:16:39 +0000

    Week 10
    Finished week 10 challenges

---
+++ Week10/SerializableTest/SerializableTest/Person.cs
@@ -0,0 +1,45 @@
+﻿namespace SerializableTest
+{
+    [Serializable]
+    public class Person
+    {
+        // fields
+        private string firstName;
+        private string lastName;
+        private int age;
+        // properties
+        public string FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        // constructors
+        public Person()
+        {
+            firstName = "unknown";
+            lastName = "unknown";
+            age = 0;
+        }
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+        // methods
+        public override string ToString()
+        {
+            return $"Name: {firstName} {lastName}, Age: {age}";
+        }
+    }
+}
---
+++ Week10/SerializableTest/SerializableTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+namespace SerializableTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John", "Smith", 21);
+            XmlSerializer serializer = new XmlSerializer(typeof(Person));
+            string fileName = "C:\\Users\\Divse\\Desktop\\Uni Imp Stuff\\YEAR 2\\COM326\\Week10\\SerializableTest/serialize.xml";
+
+            TextWriter writer = new StreamWriter(fileName);
+            serializer.Serialize(writer, person);
+            writer.Close();
+
+            FileStream fs = new FileStream(fileName, FileMode.Open);
+            Person deserializedPerson = (Person)serializer.Deserialize(fs);
+            Console.WriteLine(deserializedPerson.ToString());
+            fs.Close();
+
+        }
+    }
+}
