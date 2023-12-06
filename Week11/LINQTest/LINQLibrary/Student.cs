080ee305e238a58ff8fffbf2a8f26ff9c3c55a06
DivyanshSethi000 <Sethi-D@ulster.ac.uk>
2023-12-06 01:41:51 +0000

    Week 11 Challenge 1
    Completed challenge 1 of week 11

---
+++ Week11/LINQTest/LINQLibrary/Student.cs
@@ -0,0 +1,48 @@
+﻿namespace LINQLibrary
+{
+    public class Student
+    {
+        // fields
+        private string id;
+        private string name;
+        private int age;
+        private static int count = 1000;
+        // properties
+        public string Id
+        {
+            get { return id; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public int Count
+        {
+            get { return count; }
+        }
+        // constructors
+        public Student()
+        {
+            id = "B00"+ count++;
+            name = "unknown";
+            age = 0;
+        }
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+            id = "B00" + count++;
+        }
+        // methods
+        public override string ToString()
+        {
+            return $"Student: { id} { name} { age}";
+        }
+    }
+}
---
+++ Week11/LINQTest/LINQTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using LINQLibrary;
+namespace LINQTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>()
+            {
+             new Student("Daniel Smith", 22),
+             new Student("John Smith", 18),
+             new Student("Jane Doe", 21),
+             new Student("Joe Bloggs", 22),
+             new Student("John Doe", 23),
+             new Student("Jenny Bloggs", 20),
+             new Student("Frank Jones", 24)
+            };
+
+            var students_21 = students.Where(student => student.Age > 21);
+            var sorted_21 = students_21.OrderBy(student => student.Name);
+            var studentNames = sorted_21.Select(student => student.Name);
+            var studentGroups = students.GroupBy(student => student.Age);
+            var numGroups = studentGroups.Select(group =>
+                    new { Age = group.Key, Count = group.Count() });
+            //Another method of doing the above query
+            //var numGroups = studentGroups.Count;
+
+        }
+    }
+}
